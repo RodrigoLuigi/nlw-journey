ee2ff68f757a10cdd3a8ef4ab5f5bcee2cbd9ce3
Rodrigo Luigi <digaor@gmail.com>
2024-07-11 13:54:28 -0300

    add: openGuestsInput and closeGuestsInput , adjust eslint and add background

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -3,14 +3,20 @@ module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
 		'@rocketseat/eslint-config/react',
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
 	plugins: ['react-refresh'],
 	rules: {
-		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'prettier/prettier': ["error", {
+      'printWidth': 160,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': true,
+      'endOfLine': 'auto',
+    }],
 	}
-};
+}
